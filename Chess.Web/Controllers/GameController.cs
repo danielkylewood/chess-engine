88deb13fb87c0935465a038038dcfec5dfda1632
Daniel Wood <danielkylewood@gmail.com>
2019-01-01 17:43:16 +0000

    Partial view

--- Chess.Web/Controllers/GameController.cs
+++ Chess.Web/Controllers/GameController.cs
@@ -25,6 +25,7 @@ namespace Chess.Web.Controllers
         [HttpPost]
         public IActionResult CreateGame()
         {
+            // https://stackoverflow.com/questions/19392212/how-to-use-jquery-or-ajax-to-update-razor-partial-view-in-c-asp-net-for-a-mvc-p
             var gameState = _gameService.CreateGame(Guid.NewGuid());
             var gameStateViewModel = GameStateViewModel.FromGameState(gameState);
             return RedirectToAction("ShowGame");
@@ -44,7 +45,7 @@ namespace Chess.Web.Controllers
         public IActionResult ShowGame()
         {
             var gameState = _gameService._gameState;
-            return View("Board", GameStateViewModel.FromGameState(gameState));
+            return PartialView("Board", GameStateViewModel.FromGameState(gameState));
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
